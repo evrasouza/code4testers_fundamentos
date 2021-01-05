d5220de2295fe4ece0d559ca213f7bd484ba41ff
Everton Souza <evrasouza@gmail.com>
2021-01-04 21:41:40 -0300

    Operadores Parte 1

---
+++ ops.rb
@@ -0,0 +1,22 @@
+# Operadores Matematicos
+
+def soma(n1, n2)
+  puts n1 + n2
+end
+
+def subtrai(n1, n2)
+  puts n1 - n2
+end
+
+def multiplica(n1, n2)
+  puts n1 * n2
+end
+
+def divide(n1, n2)
+  puts (n1.to_f / n2.to_f).round(2)
+end
+
+soma(2, 3)
+subtrai(2, 3)
+multiplica(2, 3)
+divide(10, 3)
