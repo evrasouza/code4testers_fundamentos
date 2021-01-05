8a58103abf9d7b3369df455cdabd707c72f911b3
Everton Souza <evrasouza@gmail.com>
2021-01-04 21:12:01 -0300

    Variáveis e Tipos

---
+++ tipos.rb
@@ -0,0 +1,18 @@
+# Variáveis que representara Java ou CSharp
+# String nome = "Everton"
+# int idade = 37
+# boolean legal = true
+
+# Ruby não necessita de identificar o tipo da variável
+# tipagem dinâmica no ruby
+
+nome = "Everton"
+idade = 37
+legal = true
+
+puts nome
+puts nome.class
+puts idade
+puts idade.class
+puts legal
+puts legal.class
