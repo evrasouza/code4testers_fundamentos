22fb242cc8ae1cd9b20b20a6832e988d1f856c58
Everton Souza <evrasouza@gmail.com>
2021-01-04 21:25:13 -0300

    Métodos

---
+++ metodos.rb
@@ -0,0 +1,33 @@
+# padrao no java [ camelCase ]
+# padrao no CSharp [ PascalCase ]
+
+# Classes e modos no ruby usa-se [ PascalCase ]
+
+# padrao variaveis e metodos no ruby [ snakecase ]
+# Metodo criado
+# def imprime_nome(nome) #nome é um argumento
+#   puts nome
+# end
+
+# #metodo invocado
+# imprime_nome("Rafael")
+
+# def soma(valor1, valor2)
+#     puts valor1 + valor2
+# end
+
+# soma(2, 3)
+
+def pega_soma(valor1, valor2)
+  valor1 + valor2
+  # resultado = valor1 + valor2
+  # return resultado
+  # resultado
+
+  # Ruby sempre devolve a ultima instrução de código
+end
+
+total = pega_soma(2, 3)
+
+puts pega_soma(2, 3)
+puts total
