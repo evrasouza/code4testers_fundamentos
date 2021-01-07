f7d26a63f68d4955b438fc40e88075e76f09faa5
Everton Souza <evrasouza@gmail.com>
2021-01-06 21:14:40 -0300

    Controles de Fluxo

---
+++ carteira.rb
@@ -0,0 +1,12 @@
+nome = "Everton"
+idade = 6
+
+# puts idade >= 18
+
+if (idade >= 18)
+  puts nome + ", você pode tirar carteira e motorista."
+elsif (idade >= 7)
+  puts nome + ", melhor Continuar andando de bicileta"
+else
+  puts nome + ", você é muito jovem, melhor continuar andando de motoquinha"
+end
