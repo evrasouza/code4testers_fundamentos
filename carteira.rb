32b664f49cedc45c8845dfa0ba21f972e1a36f37
Everton Souza <evrasouza@gmail.com>
2021-01-09 10:25:17 -0300

    Gets e Chomp

--- carteira.rb
+++ carteira.rb
@@ -1,10 +1,13 @@
-nome = "Everton"
-idade = 6
+puts "Qual o seu nome?"
+nome = gets.chomp
+puts "Informe sua idade?"
+idade = gets.chomp.to_i
 
 # puts idade >= 18
+puts idade.class
 
 if (idade >= 18)
-  puts nome + ", você pode tirar carteira e motorista."
+  puts nome + ", você pode tirar carteira de motorista."
 elsif (idade >= 7)
   puts nome + ", melhor Continuar andando de bicileta"
 else
