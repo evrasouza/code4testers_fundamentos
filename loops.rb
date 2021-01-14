6275c66d1df296e691692ed4e0e400200042754d
Everton Souza <evrasouza@gmail.com>
2021-01-14 20:58:31 -0300

    Loops Times

---
+++ loops.rb
@@ -0,0 +1,19 @@
+5.times do
+  puts "Executando o times"
+end
+
+x = 1
+10.times do
+  puts "Executando número #{x}"
+  x = x + 1
+end
+
+bandas = ["Aerosmith", "Iron Maiden", "Bon Jovi", "Ozzy Osbourne"]
+
+puts bandas.size
+
+y = 0
+bandas.size.times do
+  puts bandas[y]
+  y = y + 1
+end
